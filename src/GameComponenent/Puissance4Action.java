652b1f363ed0ae36240f60061aaa3c2a32c9cca9
spiryti <106757618+spiryti@users.noreply.github.com>
2022-12-06 14:12:25 -0500

    Actions

--- src/GameComponenent/Puissance4Action.java
+++ src/GameComponenent/Puissance4Action.java
@@ -1,4 +1,16 @@
 package GameComponenent;
 
 public class Puissance4Action {
+    /**
+     * vaut true si c'est le joeur jaune 0 sinon
+     */
+    public boolean player;
+    /**
+     * la colonne où le jeton est joué
+     */
+    public int colonne;
+
+    Puissance4Action(boolean player,int colonne){
+        //@todo Action
+    }
 }
