e30f67f3b27d50d2a4ac947b673a60314d9d68b9
Noémie Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-12-08 12:03:26 -0500

    AlphaBeta

--- src/GameComponenent/AlphaBetaSolveur.java
+++ src/GameComponenent/AlphaBetaSolveur.java
@@ -3,16 +3,49 @@ package GameComponenent;
 public abstract class AlphaBetaSolveur<State,Action> implements Solveur<State,Action>{
 
     public Game game;
-    private int alpha;
-    private int beta;
 
-    AlphaBetaSolveur(Game game, int alpha, int beta) {
+    AlphaBetaSolveur(Game game) {
         this.game = game;
-        this.alpha = alpha;
-        this.beta = beta;
     }
     @Override
     public Action makeDecision(State state) {
         return null;
     }
+
+    public double alphaBeta(State state, int depth, boolean maximize,double alpha, double beta){
+        if (maximize) {
+            if(game.isTerminal(state)||depth==0){
+                return game.getUtility(state, false);
+            }
+            double score;
+            for (Action action : game.getActions(state)) {
+                State state2 = state;
+                state2 = game.getResult(state2,action);
+                score = alphaBeta(state2, depth-1, false,alpha,beta);
+                alpha = Math.max(alpha,score);
+                if(beta <= alpha){
+                    return alpha;
+                }
+            }
+            return alpha;
+        } else {
+            if (game.isTerminal(state) || depth == 0) {
+                return game.getUtility(state, true);
+            }
+            double score;
+            for (Action action : game.getActions(state)) {
+                State state2 = state;
+                state2 = game.getResult(state2, action);
+                score = alphaBeta(state2, depth - 1, true,alpha,beta);
+                beta = Math.min(beta,score);
+                if(beta <= alpha){
+                    return beta;
+                }
+            }
+            return beta;
+        }
+        return 0;
+    }
+
+
 }
