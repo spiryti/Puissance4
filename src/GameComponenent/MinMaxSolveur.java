c5434fd5bb47b0a5df7ef139acb94c6ba9a388ed
Noémie Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-12-07 19:06:45 -0500

    MinMax

--- src/GameComponenent/MinMaxSolveur.java
+++ src/GameComponenent/MinMaxSolveur.java
@@ -1,38 +1,36 @@
 package GameComponenent;
 
-import com.sun.source.tree.BreakTree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class MinMaxSolveur<State,Action> implements Solveur<State,Action> {
+public abstract class MinMaxSolveur<Puissance4State,Puissance4Action> implements Solveur<Puissance4State,Puissance4Action> {
 
     public Game game;
     private final int depth;
-    private static HashMap<Integer, Action> bestAction;
 
     MinMaxSolveur(Game game) {
         this.game = game;
         this.depth = 3;
-        bestAction = new HashMap<>();
     }
 
     @Override
-    public Action makeDecision(State state) {
-        Action bestaction = null;
+    public Puissance4Action makeDecision(Puissance4State state) {
+        Puissance4Action bestaction = null;
         int max = -999999;
-        for (Action action : state.getMoves()) {
-            State state2 = state.clone();
-            int score = (int) miniMax(state2,depth - 1, false).get(1);
+        HashMap<Integer, Puissance4Action> bestAction = new HashMap<>();
+        for (Puissance4Action action : state.getMoves()) {
+            Puissance4State state2 = new Puissance4State(state);
+            int score = (int) miniMax(state2,depth - 1, false, bestAction).get(1);
             if (score > max) {
-                bestaction = new Puissance4Action(action.getIndex());
+                bestaction = new Puissance4Action(action.getColonne());
                 max = score;
             }
         }
         return bestaction;
     }
 
-    public static ArrayList miniMax(State state, int depth, boolean maximize) {
+    public ArrayList miniMax(Puissance4State state, int depth, boolean maximize,HashMap<Integer, Puissance4Action> bestAction) {
         if (depth == 0) {
             ArrayList array = new ArrayList<>();
             array.add(null);
@@ -44,9 +42,9 @@ public abstract class MinMaxSolveur<State,Action> implements Solveur<State,Actio
             max.add(null);
             max.add(-999999);
 
-            for (Action action : state.getMoves()) {
-                State state2 = state.clone();
-                int score = (int) miniMax(state, depth - 1, false).get(1);
+            for (Puissance4Action action : state.getMoves()) {
+                Puissance4State state2 = new Puissance4State(state);
+                int score = (int) miniMax(state, depth - 1, false,bestAction).get(1);
                 if (score > (int) max.get(1)) {
                     max.set(0, state);
                     max.set(1, score);
@@ -57,9 +55,9 @@ public abstract class MinMaxSolveur<State,Action> implements Solveur<State,Actio
             ArrayList min = new ArrayList<>();
             min.add(null);
             min.add(999999);
-            for (Action action : state.getMoves) {
-                State state2 = state.clone();
-                int score = (int) miniMax(state2, depth - 1, true).get(1);
+            for (Puissance4Action action : state.getMoves) {
+                Puissance4State state2 = new Puissance4State(state);
+                int score = (int) miniMax(state2, depth - 1, true,bestAction).get(1);
                 if (score < (int) min.get(1))
                     min.set(0, action);
                 min.set(1, score);
