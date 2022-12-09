919e20ebe5bbaa55fed39662067e213b98f72fb9
Noémie Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-12-09 18:21:06 -0500

    Nim

--- src/MainNim.java
+++ src/MainNim.java
@@ -14,22 +14,24 @@ public class MainNim {
 
             System.out.println("Entrez le nombre de baton à enlever (entre 1 et 3)");
             int colonne = scanner.nextInt();
-            if(colonne>0 && colonne<=3) {
-                Integer nimAction = colonne;
-                nimState=nim.getResult(nimState, nimAction, joueur);
-                joueur = false;
-                System.out.println();
-                System.out.println(nimState);
-                if(!nim.isTerminal(nimState)){
-                    MinMaxSolveur<Integer,Integer> test = new MinMaxSolveur(nim);
-                    nimAction = (Integer) test.makeDecision(nimState);
+            for (Integer action : nim.getActions(nimState)) {
+                if (colonne==action) {
+                    Integer nimAction = colonne;
                     nimState = nim.getResult(nimState, nimAction, joueur);
-                    joueur = true;
+                    joueur = false;
+                    System.out.println();
+                    System.out.println(nimState);
+                    if (!nim.isTerminal(nimState)) {
+                        MinMaxSolveur<Integer, Integer> test = new MinMaxSolveur(nim);
+                        nimAction = (Integer) test.makeDecision(nimState);
+                        nimState = nim.getResult(nimState, nimAction, joueur);
+                        joueur = true;
+                    }
                 }
+                System.out.println(nimState);
             }
-            System.out.println(nimState);
         }
-        if(!joueur){
+        if(!joueur && nimState>0){
             System.out.println("Le joueur a gagné");
         }else{
             System.out.println("L'ordi a gagné");
